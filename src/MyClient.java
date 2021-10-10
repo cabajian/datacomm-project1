fb37140065038e3604c0fa42595891029ace6f97
WCJ7833 <36002848+WCJ7833@users.noreply.github.com>
2021-10-10 13:41:34 -0400

    Added more errorhandling

--- src/MyClient.java
+++ src/MyClient.java
@@ -1,27 +1,29 @@
 import java.net.*;
 import java.io.*;
 
-public class MyClient
-{
-    public static void main(String [] args)
-    {
+public class MyClient {
+    public static void main(String[] args) {
         try {
-            Socket s = new Socket( "localhost", 16789);
-            InputStream in = s.getInputStream();
-            OutputStream out = s.getOutputStream();
+            if (args.length < 1) {
+                System.out.println("No arguments entered for by the client\n");
+            } else {
+                Socket s = new Socket("localhost", 16789);
+                InputStream in = s.getInputStream();
+                OutputStream out = s.getOutputStream();
 
-            PrintWriter pout = new PrintWriter(out);
-            pout.println(args[0]);		// Writes some String to server
-            pout.flush(); 				// forces the data through to server
+                PrintWriter pout = new PrintWriter(out);
 
-            BufferedReader bin = new BufferedReader(new InputStreamReader(in));
-            System.out.println(args[0]+" <=returned-as=> " + bin.readLine());
+                pout.println(args[0]); // Writes some String to server
+                pout.flush(); // forces the data through to server
 
-            out.close();
-            pout.close();
-            s.close();
-        }
-        catch(IOException ioe) {
+                BufferedReader bin = new BufferedReader(new InputStreamReader(in));
+                System.out.println(args[0] + " <=returned-as=> " + bin.readLine());
+
+                out.close();
+                pout.close();
+                s.close();
+            }
+        } catch (IOException ioe) {
             System.out.println("IO error");
             ioe.printStackTrace();
         }
