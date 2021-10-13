e5c648eb091cf47ee666804571cb4f5fff5d7926
cabajian <abajianchris@gmail.com>
2021-10-13 14:46:12 -0400

    Added more comments.

--- src/MyClient.java
+++ src/MyClient.java
@@ -1,24 +1,36 @@
 import java.net.*;
 import java.io.*;
 
+/**
+ * MyClient class. Generates a single instance of a client, connects to the localhost
+ * server, and sends the command line arguments to the server.
+ */
 public class MyClient {
+
+    /* Server port constant. */
+    final static int SERVER_PORT = 16789;
+
+    /*
+     * Main function to connect to the server and send/receive communications.
+     */
     public static void main(String[] args) {
         try {
             if (args.length < 1) {
+                // Must have an argument to send to the server.
                 System.out.println("No arguments entered for by the client\n");
             } else {
-                Socket s = new Socket("localhost", 16789);
+                // Create the socket and IO streams.
+                Socket s = new Socket("localhost", SERVER_PORT);
                 InputStream in = s.getInputStream();
                 OutputStream out = s.getOutputStream();
-
                 PrintWriter pout = new PrintWriter(out);
-
+                // Send the first argument to the server.
                 pout.println(args[0]); // Writes some String to server
                 pout.flush(); // forces the data through to server
-
+                // Read the response from the server.
                 BufferedReader bin = new BufferedReader(new InputStreamReader(in));
                 System.out.println(args[0] + " <=returned-as=> " + bin.readLine());
-
+                // Close the connections.
                 out.close();
                 pout.close();
                 s.close();
@@ -27,6 +39,6 @@ public class MyClient {
             System.out.println("IO error");
             ioe.printStackTrace();
         }
-
     }
-}
+
+} // end MyClient
--- src/MyMultiThreadedServer.java
+++ src/MyMultiThreadedServer.java
@@ -2,88 +2,124 @@ import java.net.*;
 import java.io.*;
 import java.util.ArrayList;
 
+/**
+ * Multi-threaded Server class. Will initialize a localhost server on
+ * the constant port. Contains individual thread-server connection handlers.
+ */
 public class MyMultiThreadedServer {
+
+    /* ArrayList of string arrays to represent the student names and average grades. */
+    private ArrayList<String[]> studentGradeArray = new ArrayList<>();
+    /* The server port constant. */
+    final static int SERVER_PORT = 16789;
+
+    /*
+     *  Main function that calls the MyMultiThreadedServer constructor.
+     */
     public static void main(String[] args) {
         new MyMultiThreadedServer();
     }
 
-    private ArrayList<String[]> studentGradeArray = new ArrayList<>();
-
+    /*
+     * MyMultiThreadedServer constructor. Initializes a multi-threaded server and
+     * continuously polls for socket connections, accepts them, and creates a new thread.
+     */
     public MyMultiThreadedServer() {
         ServerSocket ss = null;
-        int SERVER_PORT = 16789;
-
         try {
+            // Create a new server socket on the port.
             ss = new ServerSocket(SERVER_PORT);
             Socket cs = null;
             System.out.println("Server started at " + SERVER_PORT);
+            // Continuously accept new clients trying to connect.
             while (true) {
-                cs = ss.accept(); // wait for connection
+                cs = ss.accept();
                 ThreadServer ths = new ThreadServer(cs);
                 ths.start();
-            } // end while
+            }
         } catch (BindException be) {
             System.out.println("Server already running on this computer, stopping.");
         } catch (IOException ioe) {
             System.out.println("IO Error");
             ioe.printStackTrace();
         }
+    }
 
-    } // end constructor
-
+    /*
+     * Adds a student name and average grade to the list as a String[] entry.
+     * This function is thread-safe and allows for concurrent execution calls.
+     */
     public synchronized void addStudentGrade(String name, double grade) {
+        // Create the string array entry.
         String[] entry = new String[2];
         entry[0] = name;
         entry[1] = String.valueOf(grade);
+        // Add the entry to the array.
         studentGradeArray.add(entry);
+        // Display the entry to the server console.
         System.out.println("Entry: " + name + ": " + grade + " added to Student Grade List");
     }
 
-    public synchronized boolean isInteger(String input) {
-        try {
-            Integer.parseInt(input);
-            return true;
-        } catch (Exception e) {
-            return false;
-        }
-    }
-
+    /**
+     * Inner class of MyMultiThreadedServer to represent a single thread on the server.
+     */
     class ThreadServer extends Thread { // member inner class
+
+        /* The socket object. */
         Socket cs;
 
+        /* ThreadServer constructor. Stores the specified socket. */
         public ThreadServer(Socket cs) {
             this.cs = cs;
         }
 
+        /* Run function that automatically executes when the thread is running.
+         * Opens I/O streams with the Client and reads an entry from the Client,
+         * parses it, stores it in the server, and sends the entry back to the Client.
+         */
         public void run() {
+            // Variable declarations.
             BufferedReader br;
             PrintWriter opw;
             String clientMsg;
             try {
+                // Open the IO streams.
                 br = new BufferedReader(new InputStreamReader(cs.getInputStream()));
                 opw = new PrintWriter(new OutputStreamWriter(cs.getOutputStream()));
-
+                // Read the message from the client and store it as a single string.
                 clientMsg = br.readLine(); // from client
+                // Split the string (comma delimited)
                 String[] splitMsg = clientMsg.split(",");
+                // Expecting a student name and three integer grades. If these conditions
+                // fail, send an error response.
                 if (splitMsg.length != 4) {
                     clientMsg = "Invalid client format length";
+                // Try processing the message.
                 } else {
                     try {
+                        // Fetch the name and compute the average grade.
                         String name = splitMsg[0];
-                        double avg = (Double.parseDouble(splitMsg[1]) + Double.parseDouble(splitMsg[2])
-                                + Double.parseDouble(splitMsg[3])) / 3.0;
+                        int a = Integer.parseInt(splitMsg[1]);
+                        int b = Integer.parseInt(splitMsg[2]);
+                        int c = Integer.parseInt(splitMsg[3]);
+                        double avg = (a+b+c)/3.0;
+                        // Add the name+grade to the server.
                         addStudentGrade(name, avg);
+                        // Send the message back.
                         clientMsg = name + "," + String.valueOf(avg);
                     } catch (NumberFormatException n) {
                         clientMsg = "Invalid format found in the client arguments";
                     }
                 }
+                // Send the (string) message back to the client and flush the output stream.
                 opw.println(clientMsg); // to client
                 opw.flush();
             } catch (IOException e) {
                 System.out.println("Inside catch");
                 e.printStackTrace();
             }
-        } // end while
+        }
+
     } // end class ThreadServer
+
 } // end MyMultiThreadedServer
